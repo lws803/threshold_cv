1ba64d86eb7c1d5735ab8f09126d55b3de42c00a
lws803 <lws803@gmail.com>
2018-09-15 01:02:24 +0800

    Added additional filter to ignore distances which are too great

--- Examples/k_nearest_detector_v2/k_nearest_detector_v2/main.cpp
+++ Examples/k_nearest_detector_v2/k_nearest_detector_v2/main.cpp
@@ -16,12 +16,23 @@
 using namespace cv;
 using namespace std;
 
+/**
+ * Three parameters to tune
+ * COLOR_SELECT lets you choose what color you wish to target
+ * DISTANCE_DIFFERENCE determines the max distance you wish to stretch the color space (makes colors more distinct)
+ * DISTANCE_LIMIT_FILTER additional filter to make sure that we only perform adaptive if it is below a certain distance
+ * OUTPUT_MODE determines which output you wish to view
+*/
+
+
 float MIN_GLOBAL, MAX_GLOBAL;
-string COLOR_SELECT = "PURE_RED";
+string COLOR_SELECT = "PURE_BLUE";
 
 class ColorMap {
     vector<float> colorArray = {0,0,0};
-    int distance_difference = 1000;
+    int DISTANCE_DIFFERENCE = 1000;
+    float DISTANCE_LIMIT_FILTER = 255; // More means larger allowance
+    
 public:
     ColorMap (string COLOR_SELECT) {
         if (COLOR_SELECT == "PURE_RED") {
@@ -44,9 +55,11 @@ public:
     vector<float> getColor () {
         return colorArray;
     }
-    
     int getDistanceDifference () {
-        return distance_difference;
+        return DISTANCE_DIFFERENCE;
+    }
+    float getDistanceLimitFilter () {
+        return DISTANCE_LIMIT_FILTER;
     }
 };
 
@@ -74,7 +87,6 @@ class pipeline {
         float difference_3 = pow((colorArray[2] - lab_channels[2]), 2);
         return sqrt(difference_2 + difference_3);
     }
-
     
     Mat preprocessor (Mat input) {
         Mat lab_image = input;
@@ -84,6 +96,29 @@ class pipeline {
         cvtColor(lab_image, lab_image, CV_BGR2Lab);
         return lab_image;
     }
+    
+    float multiply_dist (float dist) {
+        switch (FUNCTION) {
+            case QUADRATIC:
+                dist = pow(dist, MULTIPLIER);
+                break;
+            case MULTIPLICATIVE:
+                dist *= MULTIPLIER;
+                break;
+            default:
+                break;
+        }
+        return dist;
+    }
+    
+    
+    float cut_off_dist (float dist) {
+        if (dist > 255) dist = 255;
+        if (dist < 0) dist = 0;
+        
+        return dist;
+    }
+    
     
     Mat k_nearest (Mat lab_image) {
         Mat LAB[3];
@@ -102,29 +137,22 @@ class pipeline {
                     LAB[1].at<uchar>(i, d),
                     LAB[2].at<uchar>(i, d)};
                 
-                
                 float dist = cartesian_dist(myColorChoice.getColor(), lab_channels);
                 
-                switch (FUNCTION) {
-                    case QUADRATIC:
-                        dist = pow(dist, MULTIPLIER);
-                        break;
-                    case MULTIPLICATIVE:
-                        dist *= MULTIPLIER;
-                        break;
-                    default:
-                        break;
-                }
                 
-                if (dist < min) min = dist;
-                if (dist > max) max = dist;
-                
-                dist -= MIN_GLOBAL;
-                
-                
-                if (dist > 255) dist = 255;
-                if (dist < 0) dist = 0;
-                
+                if (dist >= myColorChoice.getDistanceLimitFilter()) {
+                    // If it is beyond the distance we want then just ignore.
+                    dist = 255; // Make it max
+                }else{
+                    dist = multiply_dist(dist);
+                    
+                    if (dist < min) min = dist;
+                    if (dist > max) max = dist;
+                    
+                    dist -= MIN_GLOBAL;
+                    
+                    dist = cut_off_dist(dist);
+                }                
                 img.at<uchar>(i, d) = 255 - round(dist);
             }
         }
